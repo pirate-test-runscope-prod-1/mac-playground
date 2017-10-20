893b9d805ecbd72b13b83c4af31c4afd6266ca76
Yury Nevalenny <v-yuneva@microsoft.com>
2017-10-20 16:15:38 +0300

    Initial Commit

---
+++ macPlayground/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macPlayground
+//
+//  Created by Yury Nevalenny on 20/10/2017.
+//  Copyright © 2017 Akvelon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
